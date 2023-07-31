120fe8ec09bfda57be375ec40a2c25be75dd6ded
Lu <jlu.fernandez90@gmail.com>
2023-07-31 16:44:06 +0200

    feat: app created

---
+++ app.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+
+const createError = require('http-errors');
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const passport = require('passport');
+
+/** Configurations */
+require('./config/hbs.config');
+require('./config/db.config');
+require('./config/passport.config');
+const session = require('./config/session.config');
+
+const app = express();
+
+/** View engine setup */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+/** Middlewares */
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+
+app.use(session);
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*app.use((req, res, next) => {
+  // la variable path se podrá usar desde cualquier vista de hbs (/register, /posts)
+  res.locals.path = req.path;
+  res.locals.currentUser = req.user;
+
+  // Damos paso al siguiente middleware
+  next();
+});*/
+
+/** Configure routes */
+const router = require('./config/routes.config');
+app.use('/', router);
+
+/** Error Handling */
+app.use((req, res, next) => {
+  next(createError(404, 'Page not found'));
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  const status = error.status || 500;
+
+  res.status(status).render('error', {
+    message: error.message,
+    error: req.app.get('env') === 'development' ? error : {},
+  });
+});
+
+const port = Number(process.env.PORT || 3000);
+
+app.listen(port, () => {
+  console.log(`Ready! Listening on port ${port}`);
+});

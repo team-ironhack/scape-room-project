65f0612c8deda450ca113ae246a09fb799cfdd17
Gabriela García <ggarciaborregon@gmail.com>
2023-07-31 21:42:01 +0200

    cambios

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.registerPartials(path.join(__dirname, '../views/partials'));
+
 /** Middlewares */
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));

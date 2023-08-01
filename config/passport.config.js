6bb8f7bfc6c1fc57bce465663cb8b19b7091cc7b
Lu <jlu.fernandez90@gmail.com>
2023-08-01 20:07:27 +0200

    passport hecho

---
+++ config/passport.config.js
@@ -0,0 +1,107 @@
+const passport = require('passport');
+const mongoose = require('mongoose');
+const Player = require('../models/Player');
+const Company = require('../models/Company');
+const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-auth').OAuth2Strategy;
+
+passport.serializeUser((user, next) => {
+    next(null, user._id);
+});
+
+passport.deserializeUser((id, next) => {
+    Player.findById(id)
+        .then(player => {
+            if (player) {
+                next(null, player);
+            } else {
+                Company.findById(id)
+                    .then(company => {
+                        if (company) {
+                            next(null, company);
+                        }
+                    })
+                   .catch(err => next(err));
+            }
+        })
+});
+
+passport.use(
+    'local-auth',
+    new LocalStrategy(
+        {
+            usernameField: 'email',
+            passwordField: 'password',
+        },
+        (email, password, next) => {
+            Player.findOne({ email })
+                .then(player => {
+                    if (!player) {
+                        return Company.findOne({ email })
+                            .then(company => {
+                                if (!company) {
+                                    next(null, null, { email: 'Email o contraseña inválidos.' });
+                                } else {
+                                    return company.checkPassword(password)
+                                        .then(match => {
+                                            if (!match) {
+                                                next(null, null, { email: 'Email o contraseña inválidos.' });
+                                            } else {
+                                                next(null, company);
+                                            }
+                                        });
+                                }
+                            });
+                    } else {
+                        return player.checkPassword(password)
+                            .then(match => {
+                                if (!match) {
+                                    next(null, null, { email: 'Email o contaseña inválidos.' });
+                                } else {
+                                    next(null, player);
+                                }
+                            });
+                    }
+                })
+                .catch(err => next(err));
+        }
+    )
+);
+
+passport.use(
+    'google-auth',
+    new GoogleStrategy({
+        clientID: process.env.G_CLIENT_ID,
+        clientSecret: process.env.G_CLIENT_SECRET,
+        callbackURL: process.env.G_REDIRECT_URI || '/authenticate/google/cb',
+    },
+        (accessToken, refreshToken, profile, next) => {
+            const displayName = profile.displayName;
+            const googleID = profile.id;
+            const email = profile.emails[0] ? profile.emails[0].value : undefined;
+            const avatar = profile.photos[0] ? profile.photos[0].value : undefined;
+
+            if (displayName && googleID && email) {
+                Player.findOne({ $or: [{ email }, { googleID }] })
+                    .then(player => {
+                        if (player) {
+                            next(null, player);
+                        } else {
+                            const playerData = {
+                                name: displayName,
+                                email,
+                                password: new mongoose.Types.ObjectId(),
+                                googleID,
+                                avatar,
+                            }
+                            return Player.create(playerData)
+                            .then(createdPlayer => next (null, createdPlayer))
+                        }
+                    })
+                    .catch(err => next(err))
+            } else {
+                next(null, null, { email: 'Email o contaseña inválidos.' })
+            }
+        }
+)
+);

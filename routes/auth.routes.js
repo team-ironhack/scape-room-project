b5563ecb6dbd146c746d4cd3e49fa855401c002c
Gabriela García <ggarciaborregon@gmail.com>
2023-08-02 21:18:03 +0200

    avatar

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
+const upload = require('../config/multer.config');
 
 
 router.get('/register-player', authController.registerPlayer);
-router.post('/register-player', authController.doRegisterPlayer);
+router.post('/register-player', upload.single('avatar'), authController.doRegisterPlayer);
 router.get('/register-company', authController.registerCompany);
-router.post('/register-company', authController.doRegisterCompany);
+router.post('/register-company', upload.single('avatar'), authController.doRegisterCompany);
 
 router.get('/player/:id/activate', authController.activatePlayer);
 router.get('/company/:id/activate', authController.activateCompany)

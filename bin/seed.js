b5af6076741da63f2c2310f541ad10ba38c3c1d7
Lu <jlu.fernandez90@gmail.com>
2023-08-16 19:00:17 +0200

    cambios en el modelo player

--- bin/seed.js
+++ bin/seed.js
@@ -38,13 +38,14 @@ mongoose.connection.once('open', () => {
             });
 
            
-            PLAYERS.forEach(player => {
+           /* PLAYERS.forEach(player => {
                 const randomRoomsArr = //array random de entre 1 y 3 elementos de createdRooms -> [objectId(12312312312312), ]
                 Player.create({ ...player, doneRooms: randomRoomsArr})
                     .then(createdPlayer => {
                         console.log(`The player ${createdPlayer.name} has been created`)
                     })
-            })
+            })*/
+            return Player.create(PLAYERS)
 
         })
         .then((createdPlayers) => {

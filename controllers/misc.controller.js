db172789f120e0a9d8ab1ec04934c26b9bc39d6d
Lu <jlu.fernandez90@gmail.com>
2023-08-17 21:05:08 +0200

    company prof actualizado

--- controllers/misc.controller.js
+++ controllers/misc.controller.js
@@ -1,9 +1,20 @@
+const mongoose = require('mongoose');
+const Room = require('../models/Room.model');
+const Player = require('../models/Player.model')
+
 module.exports.test = (req, res, next) => {
     res.redirect('/login')
 }
 
 module.exports.list = (req, res, next) => {
 
-    const promises = 
-    res.render('home')
+    const promises = [
+        Room.find().limit(4).sort({ createdAt: 'descending' }),
+        Player.find().limit(3).sort({ createdAt: 'descending' }),
+    ]
+    Promise.all(promises)
+        .then(([salas, jugadores]) => {
+            res.render('home', { lastRooms: salas, lastPlayers: jugadores })
+        })
+        .catch(next)
 }

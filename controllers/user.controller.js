49efc14aaf77a1c4ab631c92418c35962efab9ec
Gabriela García <ggarciaborregon@gmail.com>
2023-08-17 21:34:12 +0200

    cambios en controladores y rutas

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -49,37 +49,3 @@ module.exports.companyProfile = (req, res, next) => {
       .catch(next)
   }
 
-module.exports.createRoom = (req, res, next) => {
-    res.render('room/room-form')
-}
-
-module.exports.doCreateRoom = (req, res, next) => {
-
-  const renderWithErrors = (errors) => {
-        res.render('room/room-form', {
-            room: req.body,
-            errors
-        })
-    } 
-
-    const data = {
-        ...req.body,
-        company: req.user._id,
-        image: req.file ? req.file.path : undefined
-    }
-
-    console.log(data)
-    
-    Room.create(data)
-    .then(room => {
-      console.log(`Sala ${room.name} creada.`)
-      res.redirect(`/company/profile/${req.user._id}`)   
-    })
-    .catch(err => {
-      if (err instanceof mongoose.Error.ValidationError) {
-        renderWithErrors(err.errors);
-      } else {
-        next(err);
-      }
-      })
-}

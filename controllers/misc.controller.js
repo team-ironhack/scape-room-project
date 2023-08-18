813b8acb912b96b395307369feb6719050c38a22
Lu <jlu.fernandez90@gmail.com>
2023-08-18 23:19:27 +0200

    buscador empezado

--- controllers/misc.controller.js
+++ controllers/misc.controller.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Room = require('../models/Room.model');
-const Player = require('../models/Player.model')
+const Player = require('../models/Player.model');
+const Company = require('../models/Company.model')
 
 module.exports.test = (req, res, next) => {
     res.redirect('/login')
@@ -18,3 +19,29 @@ module.exports.list = (req, res, next) => {
         })
         .catch(next)
 }
+
+module.exports.results = (req, res, next) => {
+    const { name, company, city } = req.query
+    const criteria = {};
+  
+    if (name) {
+      criteria.name = new RegExp(name, 'i')
+    }
+    if (company) {
+      criteria.company = new RegExp(company, 'i')
+    }
+    if (city) {
+        criteria.city = new RegExp(city, 'i')
+      }
+  
+    Company.find(criteria)
+      .populate('rooms')
+      .then(companies =>
+        res.render('results', {
+            companies,
+          name,
+          company,
+          city
+        })
+      ).catch(error => next(error));
+  }

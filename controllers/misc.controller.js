23c56f89039801b556466138b908d008c6a20d7a
Lu <jlu.fernandez90@gmail.com>
2023-08-19 17:30:33 +0200

    algun cambio

--- controllers/misc.controller.js
+++ controllers/misc.controller.js
@@ -4,44 +4,49 @@ const Player = require('../models/Player.model');
 const Company = require('../models/Company.model')
 
 module.exports.test = (req, res, next) => {
-    res.redirect('/login')
+  res.redirect('/login')
 }
 
 module.exports.list = (req, res, next) => {
 
-    const promises = [
-        Room.find().populate("company").limit(4).sort({ createdAt: 'descending' }),
-        Player.find().limit(3).sort({ createdAt: 'descending' }),
-    ]
-    Promise.all(promises)
-        .then(([salas, jugadores]) => {
-            res.render('home', { lastRooms: salas, lastPlayers: jugadores })
-        })
-        .catch(next)
+  const promises = [
+    Room.find().populate("company").limit(4).sort({ createdAt: 'descending' }),
+    Player.find().limit(3).sort({ createdAt: 'descending' }),
+  ]
+  Promise.all(promises)
+    .then(([salas, jugadores]) => {
+      res.render('home', { lastRooms: salas, lastPlayers: jugadores })
+    })
+    .catch(next)
 }
 
 module.exports.results = (req, res, next) => {
-    const { companyName, company, city } = req.query
-    const criteria = {};
-  
-    if (companyName) {
-      criteria.companyName = new RegExp(companyName, 'i')
-    }
-    if (company) {
-      criteria.company = new RegExp(company, 'i')
-    }
-    if (city) {
-        criteria.city = new RegExp(city, 'i')
-      }
-  
-    Company.find(criteria)
-      .populate('rooms')
-      .then(companies =>
-        res.render('results', {
+  const { search } = req.query
+  const companyCriteria = {
+    $or: [
+      { 'city': new RegExp(search, 'i') },
+      { 'companyName': new RegExp(search, 'i') },
+      { 'province': new RegExp(search, 'i') },
+    ]
+  };
+
+  const roomCriteria = {
+    'name': new RegExp(search, 'i'),
+  };
+
+  Company.find(companyCriteria)
+    .populate('rooms')
+    .then(companies =>
+      Room.find(roomCriteria)
+        .populate('company')
+        .then(rooms =>
+          res.render('results', {
             companies,
-            companyName,
-          company,
-          city
-        })
-      ).catch(error => next(error));
-  }
+            rooms,
+            search
+          })
+        )
+        .catch(error => next(error))
+    )
+    .catch(error => next(error));
+}
